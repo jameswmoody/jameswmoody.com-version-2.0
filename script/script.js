13c646a471c44450966e612b98e32253541e1c93
James W Moody <jamesmoodyradio@gmail.com>
2017-04-23 16:30:59 -0500

    Add content sections to page template

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 window.addEventListener("scroll", function() {
   if (window.scrollY >= 50) {
     $('.navbar-default').css('background-color', '#fff');
-    // $('.navbar-default').css('border-bottom', 'solid #E5E5E5 1px');
+    $('.navbar-default').css('border-bottom', 'solid #E5E5E5 1px');
     $('.navbar-brand').css('color', '#6E6F73');
     $('.nav-link').css('color', '#6E6F73');
   }
   else {
     $('.navbar-default').css('background-color', 'transparent');
-    // $('.navbar-default').css('border-bottom', 'none');
+    $('.navbar-default').css('border-bottom', 'none');
     $('.navbar-brand').css('color', '#fff');
     $('.nav-link').css('color', '#fff');
   }
